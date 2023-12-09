6342ada26bac225264763de62d7760872f674678
JuliaWalton <134331524+JuliaWalton@users.noreply.github.com>
2023-12-09 10:58:48 -0600

    updated css for nav toggle functionality

--- script.js
+++ script.js
@@ -2,44 +2,36 @@ const sidebarBtn = document.getElementById('sidebar-toggle');
 const nav = document.getElementById('nav');
 const media = window.matchMedia('(width < 800px)');
 
-sidebarBtn.addEventListener('click', (e) => {
-    nav.style.transition = '.3s ease-in';
-    nav.classList.toggle('show-nav');
-    // if (nav.classList.contains('show-nav')) {
-    //         sidebarBtn.style.display = 'none';
-    //     } else {
-    //         sidebarBtn.style.display = 'flex';
-    //     }
-    const closeBtn = nav.querySelector('.close-btn');
-        closeBtn.addEventListener('click', () => {
-            nav.classList.remove('show-nav');
-            // sidebarBtn.style.display = 'flex';
-        })
-})
+function setupNav(e) {
+    if (e.matches) {
+        // console.log('is mobile');
+        nav.className = 'nav';
+        sidebarBtn.style.display = 'flex';
+        console.log(nav.className);
+        
+    } else {
+        // console.log('is desktop');
+        nav.className = 'desktop-nav';
+        sidebarBtn.style.display = 'none';
+    }
+}
 
-media.addEventListener('change', (e) => {
-        // console.log('window.matchMedia change = ' + e.matches);
-        setupNav(e);
-    
-})
 
+// Event Listeners
 window.addEventListener('DOMContentLoaded', (e) => {
     nav.className = 'desktop-nav';
     sidebarBtn.style.display = 'none';
 })
 
-function setupNav(e) {
-    if (e.matches) {
-        console.log('is mobile');
-        nav.className = 'nav';
-        sidebarBtn.style.display = 'flex';
-        // nav.style.transition = '.3s ease-in';
-        console.log(nav.className);
-        
-    } else {
-        console.log('is desktop');
-        nav.className = 'desktop-nav';
-        sidebarBtn.style.display = 'none';
-        console.log(nav.className);
-    }
-}
+media.addEventListener('change', (e) => {
+        setupNav(e);
+})
+
+sidebarBtn.addEventListener('click', (e) => {
+    nav.style.transition = '.3s ease-in';
+    nav.classList.toggle('show-nav');
+    const closeBtn = nav.querySelector('.close-btn');
+        closeBtn.addEventListener('click', () => {
+            nav.classList.remove('show-nav');
+        })
+})
